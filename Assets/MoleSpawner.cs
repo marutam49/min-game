4f317561c609ac9de86b2cda5e4eb1a7aff10434
marutam <3rddecoy@gmail.com>
2025-08-23 00:22:30 +0900

    wave制の実装

--- Assets/MoleSpawner.cs
+++ Assets/MoleSpawner.cs
@@ -12,24 +12,37 @@ public class MoleSpawner : MonoBehaviour
 
     public GameObject originObject;
 
+    //waveごとの各種ステータス初期値
+    //{enemyTipe, spawnInterval, numberSpawnAtOneTime}(enemyTipeは未実装)
+    float[,] waveFirstState = new float[3, 3]{
+        {1, 1, 1},
+        {2, 0.1f, 1},
+        {3, 1, 1}
+    };
+
+    float spawnInterval;
+
+    int numberSpawnAtOneTime;
+
     void Start()
     {
+        WaveUpdate();
         StartCoroutine(Spawn());
     }
 
-    
+
     void Update()
     {
 
     }
-    
+
 
     IEnumerator Spawn()
     {
         while (true)
         {
-            float spawnInterval = 1.0f / (ScoreManager.score / 20 + 1);
-            int numberSpawnAtOneTime = 1;
+            //spawnInterval = 1.0f / (ScoreManager.score / 20 + 1);
+            //numberSpawnAtOneTime = 1;
 
             yield return new WaitForSeconds(spawnInterval);
 
@@ -40,5 +53,11 @@ public class MoleSpawner : MonoBehaviour
                 Instantiate(originObject, spawnPoint, Quaternion.identity);
             }
         }
+    }
+
+    public void WaveUpdate()
+    {
+        spawnInterval = waveFirstState[WaveManager.wave - 1, 1];
+        numberSpawnAtOneTime = (int)waveFirstState[WaveManager.wave - 1, 2];        
     }
 }
